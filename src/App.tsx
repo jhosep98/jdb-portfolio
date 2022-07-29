69446ca7cbca83e294f059027953744ec0d8578d
jhosep98 <jhosep149@hotmail.com>
2022-07-28 23:40:06 -0300

    feat: init config and install wulpers library

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import { setTheme } from 'theme/defaultTheme';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { theme } = setTheme({});
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h1>Hello JDB!</h1>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
