d5734b4aaa7989508be652ea6c0a9011f31ef96c
jhosep98 <jhosep149@hotmail.com>
2022-07-28 23:00:46 -0300

    refactor: init vite

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./components/App";
-
-import "./assets/sass/style.scss";
-import "./assets/css/normalize.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
