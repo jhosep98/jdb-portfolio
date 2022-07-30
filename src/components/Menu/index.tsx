564eb748664fb014bfd632e88095b53240c9b9f3
jhosep98 <jhosep149@hotmail.com>
2022-07-30 12:52:19 -0300

    feat: menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { IconsPropsModel, Menu as WSMenu } from '@wulperstudio/cms';
+
+export interface MenuPropsModel {
+  menuList: IconsPropsModel[];
+}
+
+export const Menu: React.FC<MenuPropsModel> = ({ menuList }) => (
+  <WSMenu
+    menu="float"
+    display="flex"
+    icons={menuList}
+    logoUrl="https://source.unsplash.com/random"
+    order={3}
+    spacing={3}
+    spacingIcons={2}
+    customHeader={{
+      sx: {
+        borderRadius: '10px',
+        width: 90,
+      },
+    }}
+  />
+);
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
 import { LayoutMenuContent } from '@wulperstudio/cms';
 
+import { Menu } from 'components';
+import { ICONS_NAME } from 'helpers/icons';
 import { MainLayout } from './styled';
 
 export const HomePage: React.FC = () => (
   <MainLayout>
     <LayoutMenuContent
       columnGap={15}
-      menu={<header>Menu</header>}
+      menu={(
+        <Menu
+          menuList={[
+            {
+              text: 'Home',
+              icon: <Icon icon={ICONS_NAME.home} width="24" height="24" />,
+            },
+          ]}
+        />
+      )}
       content={<div>Home Page</div>}
     />
   </MainLayout>
