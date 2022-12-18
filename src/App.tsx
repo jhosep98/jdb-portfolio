469155aec0875b219aa8db0a2aa59d47b7c6d679
jhosep98 <jhosep149@hotmail.com>
2022-12-17 22:34:10 -0300

    style: color icons in main menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import './language/index';
+import { Language } from 'interfaces';
+import { useLocalStorage } from 'hooks';
 import { AppRouter } from 'routes/AppRouter';
 import { setTheme } from 'theme/defaultTheme';
 import { InitializerIcons } from 'helpers/icons';
@@ -14,8 +17,19 @@ const AppState = ({
 }: {
   children: React.ReactNode | Array<React.ReactNode>;
 }) => {
+  const { i18n } = useTranslation();
   const { modeTheme } = React.useContext(ThemeContext);
   const { theme } = setTheme({ mode: modeTheme });
+  const [languageFromLocalStorage] = useLocalStorage<Language>(
+    'language',
+    'en',
+  );
+
+  React.useEffect(() => {
+    if (languageFromLocalStorage) {
+      i18n.changeLanguage(languageFromLocalStorage);
+    }
+  }, [languageFromLocalStorage]);
 
   return (
     <ThemeProvider theme={theme}>
--- src/helpers/generateRoutes.tsx
+++ src/helpers/generateRoutes.tsx
@@ -11,35 +11,90 @@ import { AUTH_ROUTES } from './routes';
 export const generateRoutes = (
   navigate: NavigateFunction,
   location: Location,
-  theme?: Theme,
+  theme: Theme,
 ): Array<IconsPropsModel> => {
   const ITEMS: Array<IconsPropsModel> = [
     {
-      icon: <Icon icon={ICONS_NAME.home} color={theme?.palette.text.primary} width="24" height="24" />,
+      icon: (
+        <Icon
+          icon={ICONS_NAME.home}
+          color={
+            location.pathname === AUTH_ROUTES.home
+              ? theme.palette.primary.main
+              : theme.palette.text.secondary
+          }
+          width="24"
+          height="24"
+        />
+      ),
       text: 'Home',
       toRoute: () => navigate(AUTH_ROUTES.home),
       active: location.pathname === AUTH_ROUTES.home,
     },
     {
-      icon: <Icon icon={ICONS_NAME.person} color={theme?.palette.text.primary} width="24" height="24" />,
+      icon: (
+        <Icon
+          icon={ICONS_NAME.person}
+          color={
+           location.pathname === `/${AUTH_ROUTES.about}`
+             ? theme.palette.primary.main
+             : theme.palette.text.secondary
+          }
+          width="24"
+          height="24"
+        />
+      ),
       text: 'About',
       toRoute: () => navigate(AUTH_ROUTES.about),
       active: location.pathname === `/${AUTH_ROUTES.about}`,
     },
     {
-      icon: <Icon icon={ICONS_NAME.skills} color={theme?.palette.text.primary} width="24" height="24" />,
+      icon: (
+        <Icon
+          icon={ICONS_NAME.skills}
+          color={
+            location.pathname === `/${AUTH_ROUTES.skills}`
+              ? theme.palette.primary.main
+              : theme.palette.text.secondary
+          }
+          width="24"
+          height="24"
+        />
+      ),
       text: 'Skills',
       toRoute: () => navigate(AUTH_ROUTES.skills),
       active: location.pathname === `/${AUTH_ROUTES.skills}`,
     },
     {
-      icon: <Icon icon={ICONS_NAME.contact} color={theme?.palette.text.primary} width="24" height="24" />,
+      icon: (
+        <Icon
+          icon={ICONS_NAME.contact}
+          color={
+            location.pathname === `/${AUTH_ROUTES.contact}`
+              ? theme.palette.primary.main
+              : theme.palette.text.secondary
+          }
+          width="24"
+          height="24"
+        />
+      ),
       text: 'Contact',
       toRoute: () => navigate(AUTH_ROUTES.contact),
       active: location.pathname === `/${AUTH_ROUTES.contact}`,
     },
     {
-      icon: <Icon icon={ICONS_NAME.settings} color={theme?.palette.text.primary} width="24" height="24" />,
+      icon: (
+        <Icon
+          icon={ICONS_NAME.settings}
+          color={
+            location.pathname === `/${AUTH_ROUTES.settings}`
+              ? theme.palette.primary.main
+              : theme.palette.text.secondary
+          }
+          width="24"
+          height="24"
+        />
+      ),
       text: 'Settings',
       toRoute: () => navigate(AUTH_ROUTES.settings),
       active: location.pathname === `/${AUTH_ROUTES.settings}`,
