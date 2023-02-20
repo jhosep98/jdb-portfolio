54e09e877dfc0543620bad969a14a4b210b6542d
jhosep98 <jhosep149@hotmail.com>
2023-02-19 23:46:38 -0300

    fix: :wrench: import env variables

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/user/',
   headers: {
-    Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}`,
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
   },
 });
