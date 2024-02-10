a838f1b4a664f5505116ee3cdc796fc622bc502c
jhosep98 <jhosep149@hotmail.com>
2024-02-09 23:00:32 -0300

    feat: condition to environment variables

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/',
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    Authorization: `Bearer ${import.meta.env.PROD ? import.meta.env.GITHUB_TOKEN : import.meta.env.VITE_GITHUB_TOKEN}`,
   },
 });
