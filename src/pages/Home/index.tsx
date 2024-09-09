e40fcb8ddc8d410351bb397c2ab2428ffc3e1304
jhosep98 <jhosep149@hotmail.com>
2024-09-09 12:48:08 -0300

    feat: cta test

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typed from 'typed.js';
+import { CTAWidget } from '@wulperstudio/cta';
 import { useTranslation } from 'react-i18next';
 import { ContentRowBlock, Hero } from '@wulperstudio/cms';
 import { Box, useTheme, useMediaQuery } from '@mui/material';
@@ -37,63 +38,66 @@ export const HomePage: React.FCC = () => {
   }, []);
 
   return (
-    <BoxContent isContainer>
-      <Hero
-        sx={{
-          height: '100%',
-          display: 'grid',
-          placeItems: 'center',
-          [theme.breakpoints.down('md')]: {
-            py: '70px',
-          },
-        }}
-      >
-        <ContentRowBlock
-          firstBox={(
-            <FadeInWhenVisible>
-              <FadeInLeftWhenVisible initialX={-20}>
-                <BlockImageAnimation src={GroovyWalkAnimation} />
-              </FadeInLeftWhenVisible>
-            </FadeInWhenVisible>
-          )}
-          secondBox={(
-            <FadeInWhenVisible>
-              <FadeInLeftWhenVisible initialX={20}>
-                <Box sx={{ py: '10px' }}>
-                  <WrapperText
-                    variant="h1"
-                    component="h1"
-                    fontWeight={500}
-                    textAlign={isMqMd ? 'center' : 'left'}
-                    sx={{
-                      letterSpacing: '5px',
-                      fontFamily: 'Ubuntu',
-                      '& .typed-cursor': {
-                        color: 'primary.main',
-                      },
-                      [theme.breakpoints.down('md')]: {
-                        wordBreak: 'break-all',
-                      },
-                    }}
-                    text={(
-                      <>
-                        {t('welcome')}
-                        {' '}
-                        <Box
-                          component="span"
-                          sx={{ color: 'primary.main' }}
-                          ref={spanRef}
-                        />
-                      </>
-                    )}
-                  />
-                </Box>
-              </FadeInLeftWhenVisible>
-            </FadeInWhenVisible>
-          )}
-        />
-      </Hero>
-    </BoxContent>
+    <>
+      <BoxContent isContainer>
+        <Hero
+          sx={{
+            height: '100%',
+            display: 'grid',
+            placeItems: 'center',
+            [theme.breakpoints.down('md')]: {
+              py: '70px',
+            },
+          }}
+        >
+          <ContentRowBlock
+            firstBox={
+              <FadeInWhenVisible>
+                <FadeInLeftWhenVisible initialX={-20}>
+                  <BlockImageAnimation src={GroovyWalkAnimation} />
+                </FadeInLeftWhenVisible>
+              </FadeInWhenVisible>
+            }
+            secondBox={
+              <FadeInWhenVisible>
+                <FadeInLeftWhenVisible initialX={20}>
+                  <Box sx={{ py: '10px' }}>
+                    <WrapperText
+                      variant="h1"
+                      component="h1"
+                      fontWeight={500}
+                      textAlign={isMqMd ? 'center' : 'left'}
+                      sx={{
+                        letterSpacing: '5px',
+                        fontFamily: 'Ubuntu',
+                        '& .typed-cursor': {
+                          color: 'primary.main',
+                        },
+                        [theme.breakpoints.down('md')]: {
+                          wordBreak: 'break-all',
+                        },
+                      }}
+                      text={
+                        <>
+                          {t('welcome')}{' '}
+                          <Box
+                            component="span"
+                            sx={{ color: 'primary.main' }}
+                            ref={spanRef}
+                          />
+                        </>
+                      }
+                    />
+                  </Box>
+                </FadeInLeftWhenVisible>
+              </FadeInWhenVisible>
+            }
+          />
+        </Hero>
+      </BoxContent>
+
+      <CTAWidget cta_preference="43ced3b1-2509-47fe-9170-5281019879f6" />
+    </>
   );
 };
 
