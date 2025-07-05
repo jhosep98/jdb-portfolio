2ebf53c0613bddadb39d9af8a0adb3c643c6302b
jhosep98 <jhosep149@hotmail.com>
2025-07-05 19:23:02 -0300

    chore: verel redirects config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 // @ts-check
 import { defineConfig } from 'astro/config'
@@ -8,6 +9,7 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://jdb-portfolio.vercel.app/',
   integrations: [react(), sitemap()],
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
