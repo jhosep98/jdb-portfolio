0315de27fc446d130cb8462718c2b5d4090b9a8d
jhosep98 <jhosep149@hotmail.com>
2025-07-05 19:12:47 -0300

    chore: sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
-
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://jdb-portfolio.vercel.app/',
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
