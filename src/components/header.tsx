447b85c5be5b2daf57c9621598ad1adc3184e07a
jhosep98 <jhosep149@hotmail.com>
2025-07-24 14:07:55 -0300

    fix: load componet header in the client

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,8 +26,8 @@ export const HeroHeader: React.FC<HeroHeaderProps> = ({ children }) => {
       <nav data-state={menuState && 'active'} className='fixed z-50 w-full px-2'>
         <div
           className={cn(
-            'mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12',
-            isScrolled && 'bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5',
+            'mx-auto mt-4 px-6 transition-all duration-300 lg:px-12 bg-background/50 max-w-4xl rounded-xl border backdrop-blur-lg',
+            isScrolled && '',
           )}
         >
           <div className='relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4'>
