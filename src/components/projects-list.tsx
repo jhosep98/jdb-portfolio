7f3de98ba9980dafc58f8705d4b5d6950db692b0
jhosep98 <jhosep149@hotmail.com>
2026-08-27 19:24:02 -0300

    content: refresh projects list

--- src/components/projects-list.tsx
+++ src/components/projects-list.tsx
@@ -4,19 +4,10 @@ import IntegrationCard from './integration-card'
 
 const REPOS = [
   {
-    title: 'JM Folio',
+    title: 'OmniVest AI',
     description:
-      'JM Folio is a portfolio tracker application for managing investment positions across stocks, crypto, cash, and ETFs.',
-    link: 'https://github.com/jhosep98/jmfolio',
-    image:
-      'https://res.cloudinary.com/diusjfaoe/image/upload/v1725495136/jmdev-studio/nextjs_uzrc3s.svg',
-    alt: 'storybook-icon',
-  },
-  {
-    title: 'theme-extractor',
-    description:
-      'Extracts theme (colors, typography, etc.) from websites and provides it in a structured JSON format.',
-    link: 'https://github.com/jhosep98/theme-extractor',
+      'OmniVest AI is a personal finance ecosystem and trading journal designed to consolidate your portfolio, track derivatives trading, and leverage AI automation for smart financial insights.',
+    link: 'https://github.com/jhosep98/omnivest-ai',
     image:
       'https://res.cloudinary.com/diusjfaoe/image/upload/v1725495138/jmdev-studio/typescript_mabr0s.svg',
     alt: 'typescript',
@@ -24,11 +15,11 @@ const REPOS = [
   {
     title: 'jdb-portfolio',
     description:
-      "A modern, responsive personal portfolio website built with Astro, React, and TailwindCSS. This portfolio showcases Jhosep Davila's work as a Full Stack Developer with interactive animations and a clean, professional design.",
+      "A modern, responsive personal portfolio website built with NextJS, React, and TailwindCSS. This portfolio showcases Jhosep Davila's work as a Full Stack Developer with interactive animations and a clean, professional design.",
     link: 'https://github.com/jhosep98/jdb-portfolio',
     image:
-      'https://res.cloudinary.com/diusjfaoe/image/upload/v1725495136/jmdev-studio/astro_zisnli.svg',
-    alt: 'astro',
+      'https://res.cloudinary.com/diusjfaoe/image/upload/v1725495136/jmdev-studio/nextjs_uzrc3s.svg',
+    alt: 'nextjs',
   },
 ]
 
